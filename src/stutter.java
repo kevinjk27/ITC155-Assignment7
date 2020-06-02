91e049428fc996964ff2605ec1898d4def1776e8
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-06-02 14:48:09 -0700

    initial commit

---
+++ src/stutter.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class stutter {
+
+	public static void main(String[] args) {
+		Integer[] data = { 72,-49,30,54,-302 };
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int num : data) {
+			stack.push(num);
+		}
+		
+		stutter(stack);
+	}
+
+	public static void stutter(Stack<Integer> S) {
+		Queue<Integer> Q = new LinkedList<Integer>();
+
+		//turns it into a Queue (in reversed)
+		while (!S.isEmpty()) {
+			int n = S.pop();
+			Q.add(n);
+			Q.add(n);
+		}
+
+		
+		//turns it into a Stack (in reversed)
+		while (!Q.isEmpty()) {
+			S.push(Q.remove());
+		}
+
+		
+		//turns it into a Queue (in ordered)
+		while (!S.isEmpty()) {
+			Q.add(S.pop());
+		}
+
+		System.out.println(Q);
+	}
+
+}
