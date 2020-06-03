51fa6116f13652a92d5d18b9ae92be03a1c42eff
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-06-03 00:48:43 -0700

    updates palindrome

--- src/isPalindrome.java
+++ src/isPalindrome.java
@@ -8,42 +8,41 @@ import java.util.*;
 public class isPalindrome {
 
 	public static void main(String[] args) {
-		Integer[] data1 = { 1, 2, 3, 6, 2, 1 };
+		Integer[] data1 = { 1, 2, 9, 3, 2, 1 };
+		Integer[] data2 = { 1, 5, 9, 9, 5, 1 };
 		Queue<Integer> Q = new LinkedList<Integer>();
+		Queue<Integer> Q1 = new LinkedList<Integer>();
 		for (int num : data1) {
 			Q.add(num);
 		}
-		System.out.println(isPalindrome(Q));
+		for (int num : data2) {
+			Q1.add(num);
+		}
+		
+		
+		System.out.println(Q +" is palindrome: " +isPalindrome(Q));
+		System.out.println(Q1 +" is palindrome: " +isPalindrome(Q1));
 	}
 
 	public static boolean isPalindrome(Queue<Integer> Q) {
 		Stack<Integer> S = new Stack<Integer>();
-		Queue<Integer> Qduplicate = Q;
-		
+		int length = Q.size();
 
-		
-		
-		while (!Q.isEmpty()) {
-			S.push(Q.remove());
-		}		
-		Stack<Integer> Sduplicate = S;
-		
-	
-		
-		while (!S.isEmpty()) {
-			Q.add(S.pop());
+		for (int i = 0; i < length; i++) {
+			int n = Q.remove();
+			Q.add(n); // both Queue and Stack has the same output as the input
+			S.push(n); // both Queue and Stack has the same output as the input
 		}
 
-
-		System.out.println("Qduplicate = " +Qduplicate);
-		System.out.println("s = " +Sduplicate);
-		System.out.println("q = " +Q);
-
-		if (Q == Qduplicate) {
-			return true;
+		// Q.remove() takes the first element
+		// S.pop() takes the last element
+		for (int i = 0; i < length; i++) {
+			int element = Q.remove();
+			if (S.pop() != element) { // when the elements are different it returns false
+				return false;
+			}
+			Q.add(element); // by placing the n back to the end of the queue it ensures the iteration run for i < length
 		}
-
-		return false;
-
+		return true;
 	}
 }
