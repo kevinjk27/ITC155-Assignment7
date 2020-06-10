448f3311fbcb044fdc6f58bfd875a46672e03411
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-06-09 19:42:36 -0700

    Update JTestStutter.java

--- src/JTestStutter.java
+++ src/JTestStutter.java
@@ -7,25 +7,19 @@ class JTestStutter {
 
 	@Test
 	void testStutter() {
-		Integer[] data = { 72, -49, 30, 54, -302 };
+		Integer[] data = { 72, -4, 13, 9 };
 		Stack<Integer> stack = new Stack<Integer>();
 		for (int num : data) {
 			stack.push(num);
 		}
 
-
-
-
-
-		int[] list1 = { 72, 72, -49, -49, 30, 30, 54, 54, -302, -302 };
+		int[] list1 = { 72, 72, -4, -4, 13, 13, 9, 9 };
 		Stack<Integer> stackOutcome = new Stack<Integer>();
 		for (int num : list1) {
 			stackOutcome.push(num);
 		}
 
-		
 		Assertions.assertEquals(stackOutcome, stutter.stutter(stack));
-		
 
 	}
 
